61bfdc8d97dfc8b87085234305a514e67e0568aa
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-01-11 15:14:49 +0900

    fixed generation

--- controllers/generate.js
+++ controllers/generate.js
@@ -1,15 +1,24 @@
+var encoding = require('encoding-japanese');
 const QRCode = require('qrcode');
 const { createCanvas } = require('canvas');
+const { render } = require('ejs');
 const canvas = createCanvas(200, 200);
 const options = {};
 
 module.exports = (req, res) => {
-  // console.log(unescape(encodeURIComponent(req.body.name)));
+  var text = req.body.name;
+  // var text = encoding.convert(req.body.name, 'UTF8', 'AUTO');
+  // var detected = encoding.detect(req.body.name);
+  // console.log(detected);
+  // var text = encoding.convert(req.body.name,{
+  //   to:'UTF8',
+  //   from:'UNICODE'
+  // })
   QRCode.toCanvas(canvas, req.body.uuid, options, function (error) {
     const ctx = canvas.getContext('2d');
     ctx.font = '10px Impact';
     ctx.fillStyle = 'blue';
-    ctx.fillText(req.body.name, 15, 10);
+    ctx.fillText(text, 15, 10);
     var image_src = canvas.toDataURL();
     res.send(image_src);
   });
