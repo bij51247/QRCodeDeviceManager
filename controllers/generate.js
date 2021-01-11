a209eddf5b3a9476a44c603daa44a49539044681
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-01-11 13:01:21 +0900

    test

--- controllers/generate.js
+++ controllers/generate.js
@@ -4,13 +4,13 @@ const canvas = createCanvas(200, 200);
 const options = {};
 
 module.exports = (req, res) => {
-  // console.log(typeof (req.body.name));
-  console.log(unescape(req.body.name));
+  // console.log(unescape(req.body.name));
+  console.log(unescape(encodeURIComponent(req.body.name)));
   QRCode.toCanvas(canvas, req.body.uuid, options, function (error) {
     const ctx = canvas.getContext('2d');
     ctx.font = '10px Impact';
     ctx.fillStyle = 'blue';
-    ctx.fillText('テスト', 15, 10);
+    ctx.fillText(unescape(encodeURIComponent(req.body.name)), 15, 10);
     var image_src = canvas.toDataURL();
     res.send(image_src);
   });
