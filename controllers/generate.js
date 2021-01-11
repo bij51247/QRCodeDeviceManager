9fb12c124a123fc7f3d39d2acec289ed950d6183
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-01-11 19:37:21 +0900

    add fonts

--- controllers/generate.js
+++ controllers/generate.js
@@ -2,16 +2,18 @@ var encoding = require('encoding-japanese');
 var toSJIS = require('qrcode/helper/to-sjis');
 const QRCode = require('qrcode');
 const { createCanvas } = require('canvas');
+const { urlencoded } = require('body-parser');
 const canvas = createCanvas(200, 200);
 const options = {};
 
 module.exports = (req, res) => {
- 
+  // var text = urlencoded(req.body.name);
+   var text = req.body.name;
   QRCode.toCanvas(canvas, req.body.uuid, options, function (error) {
     const ctx = canvas.getContext('2d');
-    ctx.font = '10px';
+    ctx.font = '/Users/kimuratsutomu/QRCode_Device_Manager/.fonts/RictyDiminished-Regular.ttf';
     ctx.fillStyle = 'black';
-    ctx.fillText(req.body.name, 15, 10);
+    ctx.fillText(text, 15, 10);
     var image_src = canvas.toDataURL();
     res.send(image_src);
   });
