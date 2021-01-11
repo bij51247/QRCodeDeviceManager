2ee114ad388424f23cce9af7ccb723295fc5bec6
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-01-11 20:08:58 +0900

    finished

--- controllers/generate.js
+++ controllers/generate.js
@@ -7,12 +7,11 @@ const canvas = createCanvas(200, 200);
 const options = {};
 
 module.exports = (req, res) => {
-  // var text = urlencoded(req.body.name);
    var text = req.body.name;
   QRCode.toCanvas(canvas, req.body.uuid, options, function (error) {
     const ctx = canvas.getContext('2d');
     ctx.font = '/Users/kimuratsutomu/QRCode_Device_Manager/.fonts/RictyDiminished-Regular.ttf';
-    ctx.fillStyle = 'blue';
+    ctx.fillStyle = 'black';
     ctx.fillText(text, 15, 10);
     var image_src = canvas.toDataURL();
     res.send(image_src);
