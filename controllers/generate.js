0f7c981760ae6cb247b5273fe3ee184203740542
Tsutomu Kimura <kmrttm247810@gmail.com>
2021-01-11 15:54:14 +0900

    fixed jis

--- controllers/generate.js
+++ controllers/generate.js
@@ -1,4 +1,5 @@
 var encoding = require('encoding-japanese');
+var toSJIS = require('qrcode/helper/to-sjis');
 const QRCode = require('qrcode');
 const { createCanvas } = require('canvas');
 const { render } = require('ejs');
@@ -6,15 +7,15 @@ const canvas = createCanvas(200, 200);
 const options = {};
 
 module.exports = (req, res) => {
-  // var text = req.body.name;
-  var text = encoding.convert(req.body.name, 'JIS', 'AUTO');
+  var text = req.body.name;
+  // var text = encoding.convert(req.body.name, 'JIS', 'AUTO');
   // var detected = encoding.detect(req.body.name);
   // console.log(detected);
   // var text = encoding.convert(req.body.name,{
   //   to:'UTF8',
   //   from:'UNICODE'
   // })
-  QRCode.toCanvas(canvas, req.body.uuid, options, function (error) {
+  QRCode.toCanvas(canvas, req.body.uuid,{toSJISFunc:QRCode.toSJIS} , function (error) {
     const ctx = canvas.getContext('2d');
     ctx.font = '10px Impact';
     ctx.fillStyle = 'blue';
